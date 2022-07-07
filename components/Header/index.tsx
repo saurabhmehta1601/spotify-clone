240313ba0e43646618e91032fbd0051eecc16a67
Saurabh <57309205+saurabhmehta1601@users.noreply.github.com>
2022-07-07 07:58:02 +0000

    style: update Header component styles

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,11 +3,19 @@ import Image from "next/image";
 import React, { ComponentPropsWithoutRef } from "react";
 import styled from "@emotion/styled";
 import { DesktopNavbar } from "./DesktopNavbar";
-import NextLink from "next/link"
+import NextLink from "next/link";
 
 export const Header = (props: ComponentPropsWithoutRef<"header">) => {
   return (
-    <Flex as="header" {...props} display="flex" py="4" px="12">
+    <Flex
+      as="header"
+      {...props}
+      display="flex"
+      py="4"
+      px="12"
+      bg="black"
+      color="white"
+    >
       <Box flex="1" alignItems={"center"}>
         <NextLink href="/">
           <StyledImage width={128} height={40} src="/logo.svg" alt="logo" />
