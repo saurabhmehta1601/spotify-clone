a409d358acf06c4f2e51b025c3fcd6fa2b5c7e3e
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-07-23 10:30:59 +0530

    feat: add css-reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
+import "reset-css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../ChakraTheme";
+import { PlayerLayout } from "../components/PlayerLayout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <PlayerLayout>
+        <Component {...pageProps} />
+      </PlayerLayout>
     </ChakraProvider>
   );
 }
